a38ca4e1a8f772bbd0639b696770a78058762711
Jack Works <jackworks@protonmail.com>
2020-07-16 14:01:28 +0800

    fix: race condition

--- src/background-script.ts
+++ src/background-script.ts
@@ -1,29 +1,34 @@
 /// <reference path="../node_modules/web-ext-types/global/index.d.ts" />
 
-import { Message } from "./type"
+import { Message } from './type'
 
-const root = browser.runtime.getURL("")
-browser.runtime.onMessage.addListener(
-    (message: object, sender: browser.runtime.MessageSender) => {
-        if (!isOurMessage(message)) return undefined
-        const [, ...url] = message.url.split(root)
-        return browser.tabs
-            .executeScript(sender.tab!.id, {
-                frameId: sender.frameId,
-                file: url.join(root),
-            })
-            .then(() => {})
-    }
-)
+const root = browser.runtime.getURL('')
+const previousRequests = new Map<string, Promise<unknown>>()
+browser.runtime.onMessage.addListener((message: object, sender: browser.runtime.MessageSender) => {
+    if (!isOurMessage(message)) return undefined
+    const [, ...url] = message.url.split(root)
+    const { id } = sender.tab!
+    const { frameId } = sender
+    const key = `${id}-${frameId}`
+    const previousReq = Promise.resolve(previousRequests.get(key))
+    const p = previousReq
+        .then(() => browser.tabs.executeScript(id, { frameId, file: url.join(root) }))
+        .then(
+            () => {},
+            (e) => [e.message, e.stack]
+        )
+    previousRequests.set(key, p)
+    return p
+})
 function isOurMessage(o: object): o is Message {
     // @ts-ignore
     const parentURL = typeof o.parentURL
     return (
-        typeof o === "object" &&
+        typeof o === 'object' &&
         // @ts-ignore
-        o?.key === "system-js-loader" &&
+        o?.key === 'system-js-loader' &&
         // @ts-ignore
-        typeof o.url === "string" &&
-        (parentURL === "string" || parentURL === "undefined")
+        typeof o.url === 'string' &&
+        (parentURL === 'string' || parentURL === 'undefined')
     )
 }
--- src/content-script.ts
+++ src/content-script.ts
@@ -1,26 +1,27 @@
 /// <reference path="../node_modules/web-ext-types/global/index.d.ts" />
-import "systemjs/dist/s.js"
-import { Message } from "./type"
+import 'systemjs/dist/s.js'
+import type { Message } from './type'
 // https://github.com/systemjs/systemjs/blob/master/docs/hooks.md
 System.constructor.prototype.createScript = function () {
-    throw new Error("Unreachable case")
+    throw new Error('Unreachable case')
 }
 // System.constructor.prototype.createContext
 // System.constructor.prototype.prepareImport
-System.constructor.prototype.instantiate = function (
-    url: string,
-    parentURL: string
-) {
+System.constructor.prototype.instantiate = function (url: string, parentURL: string) {
     return browser.runtime
-        .sendMessage<Message>({ key: "system-js-loader", url, parentURL })
-        .then(() => this.getRegister())
+        .sendMessage<Message>({ key: 'system-js-loader', url, parentURL })
+        .then((e: undefined | [string, string]) => {
+            if (e) {
+                const E = new Error(e[0])
+                E.stack = e[1]
+                throw E
+            }
+            return this.getRegister()
+        })
 }
-const root = browser.runtime.getURL("/")
+const root = browser.runtime.getURL('/')
 // System.constructor.prototype.getRegister
-const origResolve: (
-    this: typeof System,
-    url: string,
-    parentURL: string
-) => string = System.constructor.prototype.resolve
+const origResolve: (this: typeof System, url: string, parentURL: string) => string =
+    System.constructor.prototype.resolve
 System.constructor.prototype.resolve = ((url, parentURL) =>
     origResolve.call(System, url, parentURL ?? root)) as typeof origResolve
